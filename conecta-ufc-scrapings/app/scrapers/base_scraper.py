272282c6d85e3de14ebf4cdb93533ec116da7ab0
Aaron Gibran <aarongibran13@gmail.com>
2026-05-06 23:43:05 -0300

    feat(scraper): melhora ufc_scraper e cria fastef_scraper com algoritmo two-pass e ghost parent

--- conecta-ufc-scrapings/app/scrapers/base_scraper.py
+++ conecta-ufc-scrapings/app/scrapers/base_scraper.py
@@ -1,7 +1,10 @@
-# app/scrapers/base_scraper.py
 import requests
 import logging
 from time import sleep
+import urllib3
+
+# Desativa os avisos de requisição insegura (já que estamos usando verify=False)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Configuração de Logs
 logging.basicConfig(
@@ -9,7 +12,6 @@ logging.basicConfig(
     level=logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
-
 
 class BaseScraper:
     def __init__(self):
@@ -22,12 +24,11 @@ class BaseScraper:
         """Tenta fazer o request com retries em caso de falha."""
         for attempt in range(retries):
             try:
-                response = requests.get(url, headers=self.headers, timeout=10)
+                response = requests.get(url, headers=self.headers, timeout=10, verify=False)
                 response.raise_for_status()
                 return response.text
             except requests.exceptions.RequestException as e:
                 self.logger.error(f"Erro ao acessar {url} (Tentativa {attempt + 1}/{retries}): {e}")
-                sleep(2)  # Espera 2 segundos antes de tentar novamente
-
+                sleep(2)
         self.logger.critical(f"Falha total ao acessar {url} após {retries} tentativas.")
         return None
